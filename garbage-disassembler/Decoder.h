d5b4f937393ce7c24a9ac94e3fe3216979b5b9cf
x32asm <x32asm@gmail.com>
2018-12-23 15:55:56 -0800

    updated

---
+++ garbage-disassembler/Decoder.h
@@ -0,0 +1,12 @@
+#ifndef GUARD_DECODER_H
+#define GUARD_DECODER_H
+#pragma once
+
+
+
+class Decoder {
+public:
+	Decoder();
+};
+
+#endif //GUARD_DECODER_H
---
+++ garbage-disassembler/Parser.h
@@ -0,0 +1,12 @@
+#ifndef GUARD_PARSER_H
+#define GUARD_PARSER_H
+#pragma once
+
+
+
+class Parser {
+public:
+	Parser();
+};
+
+#endif //GUARD_PARSER_H
